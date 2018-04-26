431a1526bf2aa62f323ffd1bac48fe6145b4fbf7
Matt Cameron <negativezero000@gmail.com>
2018-04-25 21:10:47 -0400

    Worker process for deleting read json

--- kijiji-bot.py
+++ kijiji-bot.py
@@ -10,6 +10,7 @@ import asyncio
 import aiofiles
 # Miscellaneous imports
 import logging
+import threading
 import os
 from pathlib import Path
 import json
@@ -17,12 +18,17 @@ from pprint import pprint
 import datetime
 import re
 import random
+from Queue import Queue
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+# Location for files to be deleted my a worker process
+file_bucket = Queue()
+worker_tombstone = object()
 
 class KijijiListing(object):
     '''The basic Kijiji Listing information'''
@@ -44,13 +50,14 @@ class KijijiListing(object):
 
     def to_embed(self):
         '''Created a discord embed from this instances properties'''
-        listing_embed = discord.Embed(title=self.title, description=self.description,
-                                      color=discord.Colour(random.randint(0, 16777215)),
-                                      url=self.url)
+        listing_embed = discord.Embed(
+            title=self.title, description=self.description, color=discord.Colour(random.randint(0, 16777215)),
+            url=self.url)
         listing_embed.add_field(name='Location', value=self.location, inline=True)
         listing_embed.add_field(name='Price', value=self.price, inline=True)
-        # listing_embed.set_image(url=self.imageurl)
-        # listing_embed.set_thumbnail(url='https://www.shareicon.net/data/128x128/2016/08/18/810389_strategy_512x512.png')
+        listing_embed.set_image(url=self.imageurl)
+        listing_embed.set_thumbnail(
+            url='https://www.shareicon.net/data/128x128/2016/08/18/810389_strategy_512x512.png')
         return listing_embed
 
 def kijiji_json_parse(dictionary):
@@ -71,6 +78,16 @@ def kijiji_json_parse(dictionary):
                 dictionary['postedasdate'] = datetime.datetime.utcfromtimestamp(unix_date).strftime('%B %#d, %#I:%M')
     return dictionary
 
+def file_deleter():
+    '''Monitors a queue for files. Deletes files sent to queue'''
+    while True:
+        file_path = file_bucket.get()
+        if file_path is worker_tombstone:
+            return
+        try:
+            os.remove(file_path)
+        except:
+            pass
 
 # Scripts running location. Only set if called via python.exe
 __location__ = os.path.realpath(
@@ -148,10 +165,10 @@ async def json_trawler_task():
     await bot.wait_until_ready()
     if 'listing_path' in config_options.keys() and 'posting_channel' in config_options.keys():
         json_directory = Path(config_options["listing_path"])
-
         channel = discord.Object(id=config_options["posting_channel"])
-        print("Channel:", channel)
-        print(bot.is_closed)
+
+        # Prep the deletion worker process
+
         while not bot.is_closed:
             # Iterate over the children in the directory
             print('in loop')
@@ -164,8 +181,10 @@ async def json_trawler_task():
                         listing_options = json.loads(contents, object_hook=kijiji_json_parse)
                         # Create a kijiji listing object
                         kijiji_listing = KijijiListing(dictionary=listing_options)
-                        pprint(listing_options)
+                        # Call embed method and set it to the determined channel
                         await bot.send_message(destination=channel, embed=kijiji_listing.to_embed())
+                        # Now that this file has been processed add it to the deletion queue
+                        file_bucket.put(childitem)
             except OSError as error:
                 print("'{}' is not a valid directory or is not accessible".format(json_directory))
 
@@ -177,7 +196,15 @@ async def json_trawler_task():
 if "token" in (config_options.keys()):
     # Run the bot with the supplied token
     print('Discord.py version:', discord.__version__)
+    # Start the file deleter thread
+    deleter = threading.Thread(target=file_deleter)
+    deleter.start()
+    # Start the directory monitoring task
     bot.loop.create_task(json_trawler_task())
     bot.run(config_options["token"])
+
+    # Cleanup the file deletion thread and wait for it to close
+    file_bucket.put(worker_tombstone)
+    deleter.join()
 else:
     print("Does not appear to be a key called 'token'")
